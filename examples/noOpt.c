4c6f2b1f6a6136d8d7dd2c6e435a7a3ad47bff74
Roi Castro <roi.castro.jurjo@rai.usc.es>
2023-04-25 01:29:49 +0200

    added examples folder

---
+++ examples/noOpt.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/time.h>
+
+int main(int argc, char *argv[]) {
+
+    if (argc != 3) {
+        printf("Se deben pasar 2 argumentos: ITER y N\n");
+        return 1;
+    }
+
+    int ITER = atoi(argv[1]);
+    int N = atoi(argv[2]);
+
+    struct timeval start, end;
+    gettimeofday(&start, NULL);
+
+    int i, j;
+    float a, b, c, d, e, t, u, v = 2, x, y, z;
+    for (j = 0; j < ITER; j++) {
+        for (i = 0; i < N; i++) {
+            x = i + 1.1;
+            y = j + x;
+            a = 1 / x;
+            b = 1 / (x * y);
+            c = 1 / y;
+            d = v / (x * y);
+            e = 2 * v / (x * y);
+        }
+    }
+
+    gettimeofday(&end, NULL);
+    double elapsed_time = (end.tv_sec - start.tv_sec) + (end.tv_usec - start.tv_usec) / 1000000.0;
+
+    printf("Tiempo total: %f segundos\n", elapsed_time);
+    printf("Tiempo medio por iteración: %f segundos\n", elapsed_time / (ITER * N));
+    printf("Tiempo medio por iteración del bucle N: %f segundos\n", elapsed_time / ITER);
+    printf("Valor final de las variables:\n");
+    printf("a: %f\n", a);
+    printf("b: %f\n", b);
+    printf("c: %f\n", c);
+    printf("d: %f\n", d);
+    printf("e: %f\n", e);
+    printf("-----------------------------------------------------\n");
+
+    return 0;
+}
